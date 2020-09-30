98b6547fed091ad7ca31b14bb2be0d944d4ee3d1
dcheshegorova <peschink@mail.ru>
2020-09-30 16:23:49 +0600

    Implemented loading image into mat from assets.

--- app/src/main/java/com/novikova/opencv/LoadImageActivity.kt
+++ app/src/main/java/com/novikova/opencv/LoadImageActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_load_image.*
+import org.opencv.core.Mat
+import org.opencv.core.MatOfByte
 import org.opencv.imgcodecs.Imgcodecs
 
 class LoadImageActivity: AppCompatActivity() {
@@ -15,8 +17,15 @@ class LoadImageActivity: AppCompatActivity() {
     }
 
     private fun loadImageToMatrix() {
-        val matrix = Imgcodecs.imread("247220_abstract-hdtv-wallpapers_1920x1080_h.jpg")
-        if(matrix.empty()){
+        var matrix = Mat()
+        val input = assets.open("247220_abstract-hdtv-wallpapers_1920x1080_h.jpg")
+
+        val bytes = input.readBytes()
+        val matWithBytes = MatOfByte(*bytes)
+
+        matrix = Imgcodecs.imdecode(matWithBytes, Imgcodecs.IMREAD_COLOR)
+
+        if (matrix.empty()) {
             Toast.makeText(
                 this,
                 "Не удалось загрузить изображение в матрицу!",
@@ -25,6 +34,7 @@ class LoadImageActivity: AppCompatActivity() {
             return
         }
         size.text = "${matrix.rows()} * ${matrix.cols()}"
-        matrixValues.text = matrix.dump()
+        matrixValues.text = "${matrix.channels()}"
+        sizeText2.text = "${matrix.width()} * ${matrix.height()}"
     }
 }
