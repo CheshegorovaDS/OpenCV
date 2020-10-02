dc0b8a57b867b7fd01fa27fca6ce759e1a177aaa
dcheshegorova <peschink@mail.ru>
2020-10-02 19:07:12 +0600

    Implemented draw different figure.

--- app/src/main/java/com/novikova/opencv/DrawFigureActivity.kt
+++ app/src/main/java/com/novikova/opencv/DrawFigureActivity.kt
@@ -6,21 +6,23 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_draw_figure.*
 import org.opencv.android.Utils
-import org.opencv.core.CvType
-import org.opencv.core.Mat
-import org.opencv.core.Point
-import org.opencv.core.Scalar
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 
+
 class DrawFigureActivity: AppCompatActivity() {
+    private val white = Scalar(255.0,255.0,255.0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_draw_figure)
         openMatrix(line(), imageLine)
+        openMatrix(rectangle(), imageRectangle)
+        openMatrix(circle(), imageCircle)
+        openMatrix(fillPoly(), imageFillPoly)
     }
 
     private fun line(): Mat {
-        val white = Scalar(255.0,255.0,255.0)
         val colorLine = Scalar(0.0, 255.0, 0.0)
         val mat = Mat(100, 100, CvType.CV_8UC3, white)
 
@@ -39,6 +41,73 @@ class DrawFigureActivity: AppCompatActivity() {
         )
         
         return  mat
+    }
+
+    // стрелки то же самое, но метод Imgproc.arrowedLine()
+    //+ параметр: tipLength — длина стрелки относительно длины линии (значение по умолчанию: 0.1)
+
+    private fun rectangle(): Mat {
+        val mat = Mat(100, 100, CvType.CV_8UC3, white)
+
+        val color = Scalar(0.0, 255.0, 0.0)
+
+//        Если  в  параметре thickness
+//        указать константу FILLED
+//        из класса Core, то прямоугольник будет рисоваться
+//        с заливкой без обводки.
+        val thickness = 5   //толщина обводки
+        val full = -1 //полная заливка
+        val lineType = Imgproc.LINE_AA // сглаживание
+
+
+        Imgproc.rectangle(
+            mat,
+            Point(10.0,10.0),
+            Point(80.0, 80.0),
+            color,
+            full,
+            lineType
+        )
+
+        return mat
+    }
+
+    private fun circle(): Mat {
+        val mat = Mat(100, 100, CvType.CV_8UC3, white)
+
+        val color = Scalar(0.0, 255.0, 0.0)
+        val center = Point(50.0, 50.0)
+        val radius = 50
+
+        Imgproc.circle(
+            mat,
+            center,
+            radius,
+            color
+        )
+
+        return mat
+    }
+
+    // также можно нарисовать эллипс, дугу и сектор
+
+    private fun fillPoly(): Mat {
+        val mat = Mat(100, 100, CvType.CV_8UC3, white)
+
+        val color = Scalar(0.0, 255.0, 0.0)
+
+        val points = MatOfPoint(
+            Point(0.0, 0.0),
+            Point(100.0, 0.0),
+            Point(50.0, 100.0)
+        )
+        val list = ArrayList<MatOfPoint>()
+        list.add(points)
+
+        val isClosed = true // замкнут?
+        Imgproc.fillPoly(mat, list, color)
+
+        return mat
     }
 
     private fun openMatrix(imageMatrix: Mat, imageView: ImageView) {
